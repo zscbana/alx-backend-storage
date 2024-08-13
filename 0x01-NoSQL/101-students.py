f245c21c7c54b9d48117bc41da3d32033646af5f
zscbana <zscbana@gmail.com>
2024-08-13 19:15:56 +0300

    Advanced tasks with some help

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''14. Top students
+'''
+
+
+def top_students(mongo_collection):
+    '''that returns all students sorted by average score
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
