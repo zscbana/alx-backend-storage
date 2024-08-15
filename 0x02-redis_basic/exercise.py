f3594fb77ae5782eb4015eb8e97def55b4db215a
zscbana <zscbana@gmail.com>
2024-08-15 06:59:03 +0300

    3 and 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,9 +18,42 @@ def count_calls(method: Callable) -> Callable:
     return invoker
 
 def call_history(method: Callable) -> Callable:
-    """decorator to store the history
-    of inputs and outputs for a particular function."""
-    
+    """ Stores the history of calls to a method"""
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        """ Invoker function"""
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return invoker
+
+def replay(fn: Callable) -> None:
+    """ Replay the history of a function"""
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
 
 class Cache():
     """ Cache class"""
